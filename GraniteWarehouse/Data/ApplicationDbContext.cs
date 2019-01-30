89dbe811709740cda6abfa67710ff58bf25abc25
Adam Walter <adamwalter@mail.weber.edu>
2019-01-30 11:35:03 -0700

    Finished Video 1

--- GraniteWarehouse/Data/ApplicationDbContext.cs
+++ GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GraniteWarehouse.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace GraniteWarehouse.Data
             : base(options)
         {
         }
+
+        DbSet<ProductTypes> ProductTypes { get; set; }
     }
 }
