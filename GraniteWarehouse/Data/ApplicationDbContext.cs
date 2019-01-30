9b4ffea3b97227797e4d1df2275f21f7e90df3f6
Adam Walter <adamwalter@mail.weber.edu>
2019-01-30 12:52:53 -0700

    Finished through video 2

--- GraniteWarehouse/Data/ApplicationDbContext.cs
+++ GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -14,6 +14,6 @@ namespace GraniteWarehouse.Data
         {
         }
 
-        DbSet<ProductTypes> ProductTypes { get; set; }
+        public DbSet<ProductTypes> ProductTypes { get; set; }
     }
 }
